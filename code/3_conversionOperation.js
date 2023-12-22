e586b94650daad9fd50b04a454aacb95a015ed39
Niraj kumar <112562589+kumarniraj6946@users.noreply.github.com>
2023-12-22 15:35:21 +0000

    comparision

--- code/3_conversionOperation.js
+++ code/3_conversionOperation.js
@@ -4,8 +4,8 @@ console.log(typeof score);
 console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
 
 
 // "33" => 33
@@ -17,12 +17,50 @@ let isLoggedIn = "Niraj"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
-//1 => true; 0 => false
+// 1 => true; 0 => false
 // "" => false
 // "Niraj" => true
 
-let someNumber = 33
+ let someNumber = 33
 
-let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+ let stringNumber = string(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+//   ***************************  Operations************************
+
+let negValuealue = 3
+let negValue = -Value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+ let str1 = "raj"
+ let str2 = "niraj"
+
+ let str3 = str1 + str2
+ console.log(str3);
+
+ console.log("1"+ 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+
+ //  console.log((3 + 4) * 5 % 3);
+
+ console.log(+true);
+ console.log(+"");
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 2 + 2
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+ 
